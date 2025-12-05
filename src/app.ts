5c3c731d80b7fae624c7affc1e43244b1ec8e6a5
NWilliRex <panda16520@gmail.com>
2025-12-04 22:16:39 -0500

    https fait

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,17 @@
 import express, { Application } from "express";
 import dotenv from "dotenv";
+import https from "https";
+import fs from "fs";
 import spotifyRouter from "./routes/spotifyRoutes";
 import authRouter from "./routes/authRoutes";
 import playlistRouter from "./routes/playlistRoutes";
 import { connectDB } from "./config/connectDB";
 
-
 dotenv.config();
+
 const app: Application = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 5000;
+
 app.use(express.json());
 connectDB();
 
@@ -20,6 +23,17 @@ app.get("/", (req, res) => {
   res.send("api spotifew marche");
 });
 
-app.listen(PORT, () => {
-  console.log(`api run sur 3000`);
-});
+if (fs.existsSync("./certs/key.pem") && fs.existsSync("./certs/cert.pem")) {
+  const httpsOptions = {
+    key: fs.readFileSync("./certs/key.pem"),
+    cert: fs.readFileSync("./certs/cert.pem"),
+  };
+
+  https.createServer(httpsOptions, app).listen(PORT, () => {
+    console.log(`API HTTPS lancée sur https://localhost:${PORT}`);
+  });
+} else {
+  app.listen(PORT, () => {
+    console.log(`API HTTP lancée sur http://localhost:${PORT} (certificats manquants)`);
+  });
+}
