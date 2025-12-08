4ac2834e6c0a129906aa4d69f39e473f498dfa98
yanis26x <yanis.djenadi@hotmail.com>
2025-12-08 00:49:24 -0500

    ajout de cors mtn on peut se connecter via le frontend

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 import express, { Application } from "express";
 import dotenv from "dotenv";
-import cors from "cors";                        
+import cors from "cors";
 import spotifyRouter from "./routes/spotifyRoutes";
 import authRouter from "./routes/authRoutes";
 import playlistRouter from "./routes/playlistRoutes";
@@ -8,11 +8,21 @@ import autoPlaylistRouter from "./routes/autoPlaylistRoutes";
 import { connectDB } from "./config/connectDB";
 
 dotenv.config();
+//obliger dutiliser cors
 
 const app: Application = express();
 const PORT = process.env.PORT || 4000;
 
 app.use(express.json());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+);
+
 connectDB();
 
 app.use("/spotify", spotifyRouter);
@@ -21,26 +31,9 @@ app.use("/playlists", playlistRouter);
 app.use("/autoplaylist", autoPlaylistRouter);
 
 app.get("/", (req, res) => {
-  res.send("API fonctionne HTTP sur port  " + PORT + "");
+  res.send("API fonctionne HTTP sur port " + PORT);
 });
-
+//marche pas en https ?????????
 app.listen(PORT, () => {
   console.log(`API HTTP lancée sur http://localhost:${PORT}`);
 });
-
-
-
-// if (fs.existsSync("./certs/key.pem") && fs.existsSync("./certs/cert.pem")) {
-//   const httpsOptions = {
-//     key: fs.readFileSync("./certs/key.pem"),
-//     cert: fs.readFileSync("./certs/cert.pem"),
-//   };
-
-//   https.createServer(httpsOptions, app).listen(PORT, () => {
-//     console.log(`API HTTPS lancée sur https://localhost:${PORT}`);
-//   });
-// } else {
-//   app.listen(PORT, () => {
-//     console.log(`API HTTP lancée sur http://localhost:${PORT}`);
-//   });
-// }
